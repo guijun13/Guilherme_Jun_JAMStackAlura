e0d7abed03016b8e8e68dbcefe253e5697849ca8
guijun13 <guilhermejun13.grillo@gmail.com>
2021-02-20 18:55:25 -0300

    fix(aboutwrapper): remove 'padding' from 'AboutWraper' component

--- src/components/commons/About/styles/AboutWrapper.js
+++ src/components/commons/About/styles/AboutWrapper.js
@@ -4,7 +4,7 @@ const AboutWrapper = styled.div`
   background-color: ${({ theme }) => theme.colors.background.main.color};
   border-radius: 10px;
   box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.25);
-  padding: 40px;
+  /* padding: 40px; */
 `;
 
 export default AboutWrapper;
