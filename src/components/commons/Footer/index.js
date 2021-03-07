ca751234dcde4f467eb84df67a98522e010c390e
guijun13 <guilhermejun13.grillo@gmail.com>
2021-03-07 15:26:47 -0300

    feat(footer): add 'MessageButton' with mail icon on footer

--- src/components/commons/Footer/index.js
+++ src/components/commons/Footer/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Text from '../../foundation/Text';
 import Github from '../../../theme/Github';
 import Linkedin from '../../../theme/Linkedin';
 import Facebook from '../../../theme/Facebook';
 import Instagram from '../../../theme/Instagram';
+import Mail from '../../../theme/Mail';
+import breakpointsMedia from '../../../theme/utils/breakpointsMedia';
 
 const socials = [
   {
@@ -44,6 +46,21 @@ const SocialWrapper = styled.div`
   flex-direction: row;
   justify-content: center;
   margin: 10px;
+  ${({ value }) =>
+    breakpointsMedia({
+      xs: value?.xs
+        ? css`
+            display: flex;
+            flex-direction: column;
+          `
+        : '',
+      md: value?.md
+        ? css`
+            display: flex;
+            flex-direction: row;
+          `
+        : '',
+    })}
 `;
 
 const SocialIcon = styled.a`
@@ -51,20 +68,51 @@ const SocialIcon = styled.a`
   background-color: ${({ theme }) => theme.colors.primary.main.color};
   padding: 20px;
   margin: 10px 15px;
+  transition: opacity 0.5s;
+
+  &:hover,
+  &:focus {
+    opacity: 0.7;
+  }
+`;
+
+const MessageButton = styled.button`
+  cursor: pointer;
+  border: none;
+  background-color: ${({ theme }) => theme.colors.primary.main.color};
+  color: ${({ theme }) => theme.colors.primary.main.contrastText};
+  padding: 20px;
+  margin: 10px 15px;
+  display: flex;
+  align-items: center;
+  transition: opacity 0.5s;
+
+  &:hover,
+  &:focus {
+    opacity: 0.7;
+  }
 `;
 
 export default function Footer() {
   return (
     <FooterWrapper>
-      <Text margin="20px" tag="h4" variant="titleh3">
+      <Text margin="20px" tag="h3" variant="titleh3">
         Contato
       </Text>
-      <SocialWrapper>
-        {socials.map(social => (
-          <SocialIcon href={social.url} key={social.name}>
-            {social.icon}
-          </SocialIcon>
-        ))}
+      <SocialWrapper value={{ md: 6, xs: 12 }}>
+        <MessageButton>
+          <Mail />
+          <Text margin="0 0 0 10px" tag="p" variant="regular">
+            Enviar uma mensagem
+          </Text>
+        </MessageButton>
+        <SocialWrapper>
+          {socials.map(social => (
+            <SocialIcon href={social.url} key={social.name}>
+              {social.icon}
+            </SocialIcon>
+          ))}
+        </SocialWrapper>
       </SocialWrapper>
     </FooterWrapper>
   );
