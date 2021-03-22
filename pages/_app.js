25ed04af1e435c8dc76162d8c73b4e3f4f7b767c
guijun13 <guilhermejun13.grillo@gmail.com>
2021-03-21 22:18:50 -0300

    feat(seo): add SEO title and metatags && add robots.txt

--- pages/_app.js
+++ pages/_app.js
@@ -4,19 +4,20 @@ import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
 import theme from '../src/theme/index';
 import GlobalStyle from '../src/theme/GlobalStyle';
+import SEO from '../src/components/commons/SEO';
 
 // eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Portfolio do Jun</title>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;700&display=swap"
           rel="stylesheet"
         />
       </Head>
+      <SEO headTitle="Home" />
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         {/* eslint-disable-next-line react/jsx-props-no-spreading */}
