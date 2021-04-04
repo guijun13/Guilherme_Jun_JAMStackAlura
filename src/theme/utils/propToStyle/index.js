5d2479433299bb1de104da8c591d3c779ba63b79
guijun13 <guilhermejun13.grillo@gmail.com>
2021-04-04 18:19:32 -0300

    test(prop-to-style): create 'propToStyle' unit test

---
+++ src/theme/utils/propToStyle/index.js
@@ -0,0 +1,24 @@
+// import { css } from 'styled-components';
+import breakpointsMedia from '../breakpointsMedia';
+
+export default function propToStyle(propName) {
+  return props => {
+    const propValue = props[propName];
+
+    if (typeof propValue === 'object') {
+      const breakpoints = {};
+      if (propValue.xs) breakpoints.xs = { [propName]: propValue.xs };
+      if (propValue.sm) breakpoints.sm = { [propName]: propValue.sm };
+      if (propValue.md) breakpoints.md = { [propName]: propValue.md };
+      if (propValue.lg) breakpoints.lg = { [propName]: propValue.lg };
+      if (propValue.xl) breakpoints.xl = { [propName]: propValue.xl };
+
+      return breakpointsMedia(breakpoints);
+    }
+
+    return {
+      // textAlign: props.textAlign
+      [propName]: props[propName],
+    };
+  };
+}
