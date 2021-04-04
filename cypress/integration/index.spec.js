b554d073ee5afefa41c3f6966aa183ff72bfa365
guijun13 <guilhermejun13.grillo@gmail.com>
2021-04-04 15:50:43 -0300

    test(modal-message): redistribute functions of the 'messageForm' test && add res infos validation

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -1,15 +1,48 @@
 /// <reference types="cypress" />
 
-describe('index', () => {
-  // it = teste que estamos fazendo
-  it('click on message button, fill the fields and verify success message', () => {
-    cy.visit('/');
-    cy.get('#message-button').click();
-    cy.get('#messageForm input[name="name"]').first().type('Gui');
-    cy.get('#messageForm input[name="email"]').first().type('gui@gmail.com');
-    cy.get('#messageForm textarea[name="message"]')
-      .first()
-      .type('Mensagem enviada no campo textarea');
-    cy.get('#messageForm button[type="submit"]').first().click();
+import HomeScreenPageObject from '../../src/components/screens/HomeScreen/HomeScreen.PageObject';
+
+const toBeSent = {
+  name: 'Gui',
+  email: 'gui@gmail.com',
+  message: 'Mensagem a ser mandada',
+};
+
+describe('home', () => {
+  describe('when click on message button and fill the fields', () => {
+    // it = teste que estamos fazendo
+    it('verify success message', () => {
+      /* Pre-teste */
+      // intercepta a URL
+      cy.intercept(
+        'https://contact-form-api-jamstack.herokuapp.com/message'
+      ).as('userMessage');
+
+      /* Montagem do cenário de teste */
+      const homeScreen = new HomeScreenPageObject(cy);
+      homeScreen
+        .fillMessageForm({
+          name: toBeSent.name,
+          email: toBeSent.email,
+          message: toBeSent.message,
+        })
+        .submitMessageForm();
+
+      /* Assercoes */
+      // espera a interceptação da URL
+      cy.wait('@userMessage').then(intercept => {
+        expect(intercept.response).to.have.property('statusCode', 201); // verifica se o status code é 201
+        // verifica os dados do response com os dos requests
+        expect(intercept.response.body).to.have.property('name', toBeSent.name);
+        expect(intercept.response.body).to.have.property(
+          'email',
+          toBeSent.email
+        );
+        expect(intercept.response.body).to.have.property(
+          'message',
+          toBeSent.message
+        );
+      });
+    });
   });
 });
