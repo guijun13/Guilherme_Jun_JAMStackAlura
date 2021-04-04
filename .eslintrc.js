3ccf87f539c56bec8332dbb843ec739f3d53423d
guijun13 <guilhermejun13.grillo@gmail.com>
2021-04-04 00:09:51 -0300

    feat(cypress): add 'cypress' test library and add config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,14 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    'cypress/globals': true,
   },
   extends: [
     'plugin:react/recommended',
     'airbnb',
     'prettier',
     'prettier/react',
+    'plugin:cypress/recommended',
   ],
   parserOptions: {
     ecmaFeatures: {
@@ -16,13 +18,10 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    'prettier',
-  ],
+  plugins: ['react', 'prettier'],
   rules: {
-    'react/jsx-filename-extension': [1, { "extensions": [".js", ".jsx"] }],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     // 'prettier/prettier': 'error',
-    'prettier/prettier': ["error", {"singleQuote": true, "semi": true}]
+    'prettier/prettier': ['error', { singleQuote: true, semi: true }],
   },
 };
